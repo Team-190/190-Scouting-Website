8e3e02ed4aed7ab729b443cd504bec6c2d9a04c8
asrwb0 <abhiruph.sr@gmail.com>
2025-11-20 19:12:47 -0500

    This is the function used for fetching data from a route

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,15 @@
+/*
+insert a specific link to a route containing data where it says "link"
+
+function fetchData() {
+    const route = link;
+    let data = fetch(route);
+    return data;
+}
+*/
+
+function fetchData() {
+    const route = "/teamview";
+    let data = fetch(route);
+    return data;
+}
