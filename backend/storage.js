5c29c6e22780167a9936442f5402637870a23c6f
Ruchir-Kafle <ruchirkafle@gmail.com>
2026-01-26 18:40:43 -0500

    proper config fetching

--- backend/storage.js
+++ backend/storage.js
@@ -7,9 +7,17 @@ supabaseUtil.supabaseInit()
 
 
 async function retrieveConfig(eventCode) {
+
+    let game;
+    if (eventCode.slice(0, 4) == "2025") {
+        game = "REEFSCAPE";
+    } else {
+        game = "REBUILT";
+    }
+
     const { data, error } = await supabaseClient.storage
         .from('event-configs')
-        .download(`${eventCode}-config.json`);
+        .download(`${game}.json`);
 
     return {data, error};
 }
