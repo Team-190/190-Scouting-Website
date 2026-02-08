f77dc8e31ff5df212d94188beada15ec1d4b5ae3
CommandTech <lpbachman@wpi.edu>
2026-02-08 16:03:41 -0500

    Ignores Match_Event lines for team view

--- backend/database.js
+++ backend/database.js
@@ -38,61 +38,11 @@ async function getTeamNumbers() {
 async function allData(eventCode) {
     let query = supabaseClient
         .from(eventCode)
-        .select("*");
+        .select("*")
+        .neq("RecordType", "Match_Event");
     const result = await query;
     return result;
 }
-
-
-async function oldTeamView(eventCode, teamNumber) {
-    console.log("About to call");
-    const {data, error} = await storage.retrieveConfig(eventCode);
-
-    if (parseInt(process.env.USE_CUSTOM_CONFIG)) {
-        try {
-            const raw = await fs.readFile(`test/${eventCode}-config.json`, 'utf8');
-            config = JSON.parse(raw);
-        } catch (error) {
-            return error;
-        }
-    } else {
-        if (!error) config = JSON.parse(await data.text()) 
-        else return error;
-    }
-
-    console.log(config);
-
-    for (let i = 0; i < config.teamView.length; i++) {
-        console.log(`Making new query for ${config.teamView[i].columns}`)
-        let query = supabaseClient
-            .from(eventCode)
-            .select(config.teamView[i].columns.toString());
-
-        console.log(`Adding. ${JSON.stringify(config.teamView[i].requirements)}`);
-        
-        for (const obj of config.teamView[i].requirements) {
-            for (const [key, value] of Object.entries(obj)) {
-                console.log(key, value);
-                query = query.eq(key, value);
-            }
-        }
-        query = query.eq("Team", `frc${teamNumber}`);
-
-        const result = await query;
-        result.teamNumber = teamNumber
-        // console.log(result);
-
-        // matches = {};
-
-        // for (let row of result.data) {
-        //     matches[row['Match']] = matches[row['Match']] || [];
-        //     matches[row['Match']].push(row);
-        // }
-
-        // console.log(matches);
-        return result;        
-    }
-};
 
 
 async function allTeamsView(eventCode) {
